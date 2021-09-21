19ce4d207ef1de710f4758a1c3cd3a523df6f878
Artyom Karpetc <akarpetc@kazzinc.kz>
2021-09-21 15:19:12 +0600

    Добавил имя и фамилию

--- DeeFlat.IS4/DeeFlat.IS4.Core/Domain/ApplicationUser.cs
+++ DeeFlat.IS4/DeeFlat.IS4.Core/Domain/ApplicationUser.cs
@@ -5,5 +5,9 @@ namespace DeeFlat.IS4.Core.Domain
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
     }
 }
