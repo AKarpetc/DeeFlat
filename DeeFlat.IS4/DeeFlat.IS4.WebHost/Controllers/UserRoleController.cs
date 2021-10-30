cb0f06db9ef3118bbeee5238b35a3965c59e1702
Артем <karpetsartyom@outlook.com>
2021-10-30 21:31:37 +0600

    Добавил контракт api на добавление ролей

---
+++ DeeFlat.IS4/DeeFlat.IS4.WebHost/Controllers/UserRoleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeeFlat.IS4.WebHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRoleController : ControllerBase
+    {
+        public async Task<ActionResult> GetRoles()
+        {
+            return Ok();
+        }
+
+        public async Task<ActionResult> GetFreeRoles(Guid userId)
+        {
+            return Ok();
+        }
+
+        public async Task<ActionResult> GetUserRoles(Guid userId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddRole(Guid userId)
+        {
+            return Ok();
+        }
+    }
+}
