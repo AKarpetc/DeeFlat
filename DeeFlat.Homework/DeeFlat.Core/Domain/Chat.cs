90c905ffa8d7b253363dd77f0aed0ec623de1219
silverful <deadhant14@hotmail.com>
2021-11-19 01:40:41 +0300

    Добавлен проект Core, начальные классы для чатов

---
+++ DeeFlat.Homework/DeeFlat.Core/Domain/Chat.cs
@@ -0,0 +1,12 @@
+﻿using DeeFlat.Abstractions.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace DeeFlat.Homework.Core.Domain
+{
+    public class Chat : BaseEntity
+    {
+        public IEnumerable<Guid> ChatParticipants { get; set; }
+        
+    }
+}
---
+++ DeeFlat.Homework/DeeFlat.Core/Domain/ChatMessage.cs
@@ -0,0 +1,12 @@
+﻿using DeeFlat.Abstractions.Abstractions;
+using System;
+
+namespace DeeFlat.Homework.Core.Domain
+{
+    public class ChatMessage : BaseEntity
+    {
+        public Guid FromUserId { get; set; }
+        public Guid ToChatId { get; set; }
+        public string Content { get; set; }
+    }
+}
