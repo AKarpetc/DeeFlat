11bb05788cf1f6369db725f02559af9b6ed6e3e1
silverful <deadhant14@hotmail.com>
2021-11-23 01:59:57 +0300

    Тестовая демо-версия чата

---
+++ DeeFlat.Homework.WebHost/NameUserIdProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DeeFlat.Homework.WebHost
+{
+    public class NameUserIdProvider : IUserIdProvider
+    {
+        public string GetUserId(HubConnectionContext connection)
+        {
+            return connection.User?.Identity?.Name;
+        }
+    }
+}
