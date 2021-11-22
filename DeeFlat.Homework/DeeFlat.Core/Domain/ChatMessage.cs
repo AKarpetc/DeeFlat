d7d75956a4847ec3086b5477a15dd7f27b0be073
silverful <deadhant14@hotmail.com>
2021-11-23 01:58:55 +0300

    В сообщении используется CourseId + FromUserId для идентификации сообщения

--- DeeFlat.Homework/DeeFlat.Core/Domain/ChatMessage.cs
+++ DeeFlat.Homework/DeeFlat.Core/Domain/ChatMessage.cs
@@ -6,7 +6,9 @@ namespace DeeFlat.Homework.Core.Domain
     public class ChatMessage : BaseEntity
     {
         public Guid FromUserId { get; set; }
-        public Guid ToChatId { get; set; }
+        public Guid CourseId { get; set; }
         public string Content { get; set; }
+        public int MessageIndex { get; set; }
+
     }
 }
