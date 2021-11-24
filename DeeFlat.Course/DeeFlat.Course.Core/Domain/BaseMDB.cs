52afd106c3c2f324be9ff121c9fc3bd23d9932fe
Артем <karpetsartyom@outlook.com>
2021-11-24 21:38:44 +0600

    Добавил базовый класс для MongoDB

---
+++ DeeFlat.Course/DeeFlat.Course.Core/Domain/BaseMDB.cs
@@ -0,0 +1,14 @@
+﻿using DeeFlat.Abstractions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeeFlat.Course.Core.Domain
+{
+    public class BaseMDB : BaseEntity
+    {
+        public override Guid Id { get; set; }
+    }
+}
