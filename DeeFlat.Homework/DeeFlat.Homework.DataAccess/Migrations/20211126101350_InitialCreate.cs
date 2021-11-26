e56a8b204721e45b9dae382cef089133534dee08
silverful <deadhant14@hotmail.com>
2021-11-26 13:15:12 +0300

    Добавил миграции

---
+++ DeeFlat.Homework/DeeFlat.Homework.DataAccess/Migrations/20211126101350_InitialCreate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DeeFlat.Homework.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chat",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PupilId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AssignedTeacherId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chat", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatMessage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FromUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    MessageIndex = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatMessage", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chat");
+
+            migrationBuilder.DropTable(
+                name: "ChatMessage");
+        }
+    }
+}
---
+++ DeeFlat.Homework/DeeFlat.Homework.WebHost/Hubs/ChatHub.cs
@@ -0,0 +1,90 @@
+﻿using DeeFlat.Abstractions.Repositories;
+using DeeFlat.Homework.Core.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using SignalRSwaggerGen.Attributes;
+using SignalRSwaggerGen.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeeFlat.Homework.WebHost.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        private IRepository<Chat> _chatResitory;
+        private IRepository<ChatMessage> _chatMessagesRepository;
+
+        public ChatHub(IRepository<Chat> chatResitory, 
+            IRepository<ChatMessage> chatMessagesResository)
+        {
+            _chatResitory = chatResitory;
+            _chatMessagesRepository = chatMessagesResository;
+        }
+
+        public async Task<IEnumerable<ChatMessage>> JoinChatAsPupil(string userId, string courseId)
+        {
+            if (Guid.TryParse(userId, out Guid parsedUserId) 
+                || Guid.TryParse(courseId, out Guid parsedCourseId))
+            {
+                throw new ArgumentNullException("UserId and CourseId are null");
+            }
+
+            var chat = await _chatResitory.GetFirstWhereAsync(c => c.CourseId == parsedCourseId && c.PupilId == parsedUserId);
+
+            if (chat == null)
+            {
+                chat = new Chat() { CourseId = parsedCourseId, PupilId = parsedUserId };
+                await _chatResitory.CreateAsync(chat);
+            }
+            
+            var messages = (await _chatMessagesRepository.GetWhereAsync(cm => cm.CourseId == parsedCourseId))
+                .OrderBy(m => m.MessageIndex)
+                .ToList();
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, chat.Id.ToString());
+
+            return messages;
+        }
+
+
+        public async Task SendMessageAsync(string fromUserId, string courseId, string content)
+        {
+            if (Guid.TryParse(fromUserId, out Guid parsedUserId)
+                || Guid.TryParse(courseId, out Guid parsedCourseId))
+            {
+                throw new ArgumentNullException("fromUserId and courseId are null");
+            }
+
+            var chat = await _chatResitory.GetFirstWhereAsync(c => c.CourseId == parsedCourseId && c.PupilId == parsedUserId);
+
+            if (chat.PupilId != parsedUserId) throw new Exception("You cannot use this chat");
+
+            var chatMessage = new ChatMessage() { CourseId = parsedCourseId, FromUserId = parsedUserId, Content = content };
+
+            await _chatMessagesRepository.CreateAsync(chatMessage);
+
+            await Clients.Group(chat.Id.ToString()).SendAsync("ReceiveChatMessage", chatMessage);
+        }
+
+        public async Task SendMessageAsTeacherAsync(string fromUserId, string courseId, string content)
+        {
+            if (Guid.TryParse(fromUserId, out Guid parsedUserId)
+                || Guid.TryParse(courseId, out Guid parsedCourseId))
+            {
+                throw new ArgumentNullException("fromUserId and courseId are null");
+            }
+
+            var chat = await _chatResitory.GetFirstWhereAsync(c => c.CourseId == parsedCourseId && c.PupilId == parsedUserId);
+
+            chat.AssignedTeacherId = parsedUserId;
+            var chatMessage = new ChatMessage() { CourseId = parsedCourseId, FromUserId = parsedUserId, Content = content };
+
+            await _chatResitory.UpdateAsync(chat);
+
+            await Clients.Group(chat.Id.ToString()).SendAsync("ReceiveChatMessage", chatMessage);
+        }
+    }
+}
