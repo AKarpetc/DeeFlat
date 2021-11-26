0e041dba27568447287c000ddc02c95f2b0a8924
silverful <deadhant14@hotmail.com>
2021-11-26 13:44:00 +0300

    Добавлен сервис для оповещения о выполненной домашней работе

---
+++ DeeFlat.Homework/DeeFlat.Homework.Services/Contracts/HomeworkAccepted.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DeeFlat.Homework.Services.Contracts
+{
+    public class HomeworkAccepted
+    {
+        public HomeworkAccepted(Guid courseId, Guid pupilId, Guid teacherId)
+        {
+            CourseId = courseId;
+            PupilId = pupilId;
+            TeacherId = teacherId;
+        }
+
+        public Guid CourseId { get; set; }
+        public Guid PupilId { get; set; }
+        public Guid TeacherId { get; set; }
+    }
+}
---
+++ DeeFlat.Homework/DeeFlat.Homework.Services/Producers/HomeworkProducer.cs
@@ -0,0 +1,23 @@
+﻿using DeeFlat.Homework.Services.Contracts;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace DeeFlat.Homework.Services.Producers
+{
+    public class HomeworkProducer : IHomeworkProducer
+    {
+        readonly IBus _bus;
+        public HomeworkProducer(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task SendHomeworkAcceptedAsync(Guid courseId, Guid pupilId, Guid teacherId)
+        {
+            var message = new HomeworkAccepted(courseId, pupilId, teacherId);
+
+            await _bus.Publish(message);
+        }
+    }
+}
