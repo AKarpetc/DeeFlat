7d08fa8080da4d6b67a4860c176e8caf6f443892
Артем <karpetsartyom@outlook.com>
2021-11-26 21:44:27 +0600

    Добавил собственный обработчик ошибок и логгирование всех ошибок в middlewere

---
+++ DeeFlat.Abstractions/Exceptions/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeeFlat.Abstractions.Exceptions
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        private static Task HandleExceptionMessageAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            var result = JsonConvert.SerializeObject(new
+            {
+                StatusCode = statusCode,
+                ErrorMessage = exception.Message
+            });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+            return context.Response.WriteAsync(result);
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                await HandleExceptionMessageAsync(context, ex).ConfigureAwait(false);
+            }
+        }
+    }
+}
